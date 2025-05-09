2279180732355336c998e6ee70a4216a013bb5ba
Zehryx <zehryx.vaeles@gmail.com>
2025-05-08 21:26:28 -0700

    Added new brands

--- src/data/brands.js
+++ src/data/brands.js
@@ -13,12 +13,45 @@ export default [
   {
     brandId: "002",
     name: "Tsukiya",
-    description: "The vibrant world of anime to life through high-quality anime cases",
+    description: "The vibrant world of anime to life through high-quality anime cases.",
     tags: ["anime", "accessory"],
     usMade: "no",
     origin: "",
     image: "https://via.placeholder.com/400x300?text=Velvet+%26+Bloom",
     affiliateLink: "https://tsukiyashop.com/",
     websiteDescription: "Founded by a group of anime enthusiasts, TSUKIYA was born from a shared love for the art, stories, and culture of anime. We saw a need for a dedicated space where fans could not only find the best anime cases but also connect with others who share their passion. Since our inception, we've been committed to delivering an exceptional experience to our customers and fostering a vibrant, inclusive community."
+  },
+  {
+    brandId: "003",
+    name: "Stitch Spring",
+    description: "Anime iconography reimagined as comfort pieces.",
+    tags: ["anime", "clothing"],
+    usMade: "no",
+    origin: "",
+    image: "https://via.placeholder.com/400x300?text=Velvet+%26+Bloom",
+    affiliateLink: "https://stitchspring.com/",
+    websiteDescription: ""
+  },
+  {
+    brandId: "004",
+    name: "SparkTrendz",
+    description: "Rock fresh style for your favorite anime.",
+    tags: ["anime", "clothing"],
+    usMade: "no",
+    origin: "",
+    image: "https://via.placeholder.com/400x300?text=Velvet+%26+Bloom",
+    affiliateLink: "https://stitchspring.com/",
+    websiteDescription: "SparkTrendz is an anime streetwear brand based in Los Angeles. Our journey started with a viral hoodie design on TikTok and since then, we've put our focus on giving fans a way to express their love for anime through design and fashion!"
+  },
+  {
+    brandId: "005",
+    name: "Grandlinevault",
+    description: "High-quality, Anime-inspired clothing that celebrate the adventurous spirit of this beloved anime.",
+    tags: ["anime", "clothing"],
+    usMade: "no",
+    origin: "",
+    image: "https://via.placeholder.com/400x300?text=Velvet+%26+Bloom",
+    affiliateLink: "https://grandlinevault.com/",
+    websiteDescription: "We create stylish designs that embody the essence of the Straw Hat Pirates while staying current with fashion trends. Our goal is to empower fans to express their love for One Piece through wearable pieces that fit seamlessly into everyday life, \"One Piece\" at a time :)"
   }
 ];
